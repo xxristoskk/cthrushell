90e573adb140e2b2ff9c348eafc5de42da87c3b8
xxristoskk <katsaros.christopher@gmail.com>
2022-06-03 19:41:31 -0400

    heroku update

--- cthrushell/settings.py
+++ cthrushell/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cthrushell.herokuapp', 'herokuapp', 'heroku', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp', '127.0.0.1']
 
 
 # Application definition
