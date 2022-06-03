c8b1af10d8fe0a02c00fc89a5d89de8800b81fc2
xxristoskk <katsaros.christopher@gmail.com>
2022-06-03 19:26:32 -0400

    heroku update

--- cthrushell/settings.py
+++ cthrushell/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'cthrushell/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'cthrushell/static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'cthrushell/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'cthrushell/static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
