e16f1d6a4811a29c7b21eb80cfd735b992d46dde
xxristoskk <katsaros.christopher@gmail.com>
2022-07-27 12:33:04 -0400

    removed/added art

--- cthrushell/settings.py
+++ cthrushell/settings.py
@@ -125,10 +125,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'cthrushell/static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'cthrushell/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cthrushell/static'),
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,9 @@ app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/glitch-art', glitch_art, name='glitch_art'),
-    path('/ai-art', ai_art, name='ai_art'),
-    path('/music', music, name='music'),
-    path('/about', about, name='about'),
-    path('/data-dev', data_dev, name='data_dev')
+    path('glitch-art', glitch_art, name='glitch_art'),
+    path('ai-art', ai_art, name='ai_art'),
+    path('music', music, name='music'),
+    path('about', about, name='about'),
+    path('data-dev', data_dev, name='data_dev')
 ]
