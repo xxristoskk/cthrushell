469f32dcac4190175891aaf51dd46811bc3848c3
xxristoskk <katsaros.christopher@gmail.com>
2022-10-05 18:11:37 -0400

    static update

--- cthrushell/settings.py
+++ cthrushell/settings.py
@@ -22,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'django-insecure-m$(-+lu$4kbqi2@y%bptcko9h&i#7*po(cwtlj$$3-@-pdti!*'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
